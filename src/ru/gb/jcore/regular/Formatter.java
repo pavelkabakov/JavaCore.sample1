84fcc6b49d73e0e10b77908732519eb399d1867a
Павел Кабаков <pavelkabakov@gmail.com>
2024-01-28 18:38:23 +0300

    init

---
+++ src/ru/gb/jcore/regular/Formatter.java
@@ -0,0 +1,12 @@
+package ru.gb.jcore.regular;
+
+/**
+ * formatter
+ */
+
+public class Formatter {
+    public static String formatResult(String operation, double a, double b, double result) {
+        return String.format("%s of %.2f and %.2f is %.2f", operation, a, b, result);
+    }
+}
+
